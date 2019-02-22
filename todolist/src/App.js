ebb13b3076020c43307fd1fdf708408af03b9267
Oriel <1102386151@qq.com>
2019-02-22 18:10:04 +0800

    new

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
 
 class App extends Component {
     render() {
@@ -12,20 +12,53 @@ class App extends Component {
     }
 }
 
-class TodoItems extends React.Component{
-    constructor(props){
+class CreateTasks extends React.Component {
+    constructor(props) {
         super(props);
+        this.delSelf = this.delSelf.bind(this);
+        this.isChecked = this.isChecked.bind(this);
 
     }
+
+    delSelf() {
+        let data = this.props.data;
+        this.props.del(data);
+    }
+
+    isChecked(e) {
+        let data = this.props.data;
+        this.props.checked(data);
+    }
+
     render(){
+        let data = this.props.data;
+        let clear = {
+            clear:"both"
+        };
+        return <li key={data.key}>
+            <input type="checkbox" value={data.checked} checked={data.checked} onChange={(e) => {this.isChecked(e)}}/>
+            <span>{data.text}</span>
+            <button onClick={this.delSelf}>x</button>
+            <div style={clear}></div>
+        </li>
+    }
+}
+
+class TodoItems extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
         let todoEntries = this.props.entries;
-        function createTasks(item){
-            return <li key={item.key}>{item.text}</li>
-        }
-        let listItems = todoEntries.map(createTasks);
+        let del = this.props.delItem;
+        let checked = this.props.checkedItem;
+
         return (
             <ul className='theList'>
-                {listItems}
+                {
+                    todoEntries.map((item) => <CreateTasks checked={checked} del={del} data={item} key={item.key}/>)
+                }
             </ul>
         )
     }
@@ -35,16 +68,20 @@ class TodoList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: []
+            items: [],
+            finished: 0
         };
-        this.addItem = this.addItem.bind(this)
+        this.addItem = this.addItem.bind(this);
+        this.delItem = this.delItem.bind(this);
+        this.checkedItem = this.checkedItem.bind(this);
     }
 
     addItem(e) {
         const itemArray = this.state.items;
         itemArray.push({
             text: this._inputElement.value,
-            key: Date.now()
+            key: Date.now(),
+            checked: false
         });
         this.setState({
             items: itemArray
@@ -55,17 +92,50 @@ class TodoList extends React.Component {
 
     }
 
+    delItem(item) {
+        const itemArray = this.state.items;
+        let obj = [], sum = 0;
+        itemArray.forEach((value) => {
+            if (value.key !== item.key) {
+                obj.push(value);
+                if (item.checked === true) {
+                    sum++;
+                }
+            }
+        });
+        this.setState({
+            items: obj,
+            finished: sum
+        });
+    }
+
+    checkedItem(item) {
+        const itemArray = this.state.items;
+        let sum = 0;
+        itemArray.forEach((value) => {
+            if (value.key === item.key) {
+                value.checked = !item.checked;
+            }
+            if (value.checked === true) {
+                sum++;
+            }
+            }
+        );
+        this.setState({
+            finished: sum
+        });
+    }
+
     render() {
         return (
             <div className='todoListMain'>
                 <div className='header'>
-                    <form>
-                        <input ref={(a) => this._inputElement = a} placeholder="enter task">
-                        </input>
-                    </form>
+                    <input ref={(a) => this._inputElement = a} placeholder="enter task">
+                    </input>
                     <button onClick={this.addItem}>add</button>
                 </div>
-                <TodoItems entries={this.state.items} />
+                <TodoItems checkedItem={this.checkedItem} delItem={this.delItem} entries={this.state.items}/>
+                <div>总数/已完成：{this.state.items.length}/{this.state.finished}</div>
             </div>
         )
     }
