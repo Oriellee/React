516a6cd241290a3a08070f6726ac4e451d052eb9
Oriel <1102386151@qq.com>
2019-03-14 09:47:39 +0800

    new

--- comment/src/CommentList.js
+++ comment/src/CommentList.js
@@ -1,10 +1,16 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
+import Comment from './Comment'
 
-class CommentList extends Component{
-    render(){
-        return(
-            <div>CommentList</div>
+class CommentList extends Component {
+    static comments = [];
+
+    render() {
+        return (
+            <div>{this.props.comments.map((comment, i) =>
+                <Comment key={i} comment={comment}/>
+            )}</div>
         )
     }
 }
+
 export default CommentList
