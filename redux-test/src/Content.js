59558d7a9e514b2afb13319808bfd9d40fa7acb5
OrielYan <oriel@edzdeMacBook-Pro-7.local>
2019-03-19 19:34:39 +0800

    new

---
+++ redux-test/src/Content.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ThemeSwitch from './ThemeSwitch'
+
+class Content extends Component {
+    render() {
+        return (
+            <div>
+                <h1>React.js小书内容</h1>
+                <ThemeSwitch />
+            </div>
+
+        )
+    }
+}
+
+export default Content
