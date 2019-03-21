f19ec491eeb41602b518e8571bb5d1b84e7ce27d
OrielYan <oriel@edzdeMacBook-Pro-7.local>
2019-03-21 17:00:37 +0800

    自己动手制作的redux。

--- redux-test/src/ThemeSwitch.js
+++ redux-test/src/ThemeSwitch.js
@@ -1,40 +1,40 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from './react-redux'
 
 class ThemeSwitch extends Component {
     static contextTypes = {
         store: PropTypes.object
     }
-    constructor() {
-        super()
-        this.state = { themeColor: '' }
-    }
-    componentWillMount() {
-        this._updateThemeColor()
-        const {store} = this.context
-        store.subscribe(() => this._updateThemeColor())
-    }
-    _updateThemeColor() {
-        const { store } = this.context
-        const state = store.getState()
-        this.setState({
-            themeColor: state.themeColor
-        })
-    }
+
     handleSwitchColor(color) {
-        const { store } = this.context
-        store.dispatch({
-            type: 'CHANGE_COLOR',
-            themeColor: color
-        })
+        if (this.props.onSwitchColor) {
+            this.props.onSwitchColor(color)
+        }
     }
     render() {
         return (
             <div>
-                <button onClick={this.handleSwitchColor.bind(this, 'red')} style={{ color: this.state.themeColor }}>red</button>
-                <button onClick={this.handleSwitchColor.bind(this, 'blue')} style={{ color: this.state.themeColor }}>blue</button>
+                <button onClick={this.handleSwitchColor.bind(this, 'red')} style={{ color: this.props.themeColor }}>red</button>
+                <button onClick={this.handleSwitchColor.bind(this, 'blue')} style={{ color: this.props.themeColor }}>blue</button>
             </div>
         )
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        themeColor: state.themeColor
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSwitchColor: (color) => {
+            dispatch({ type: 'CHANGE_COLOR', themeColor: color })
+        }
+    }
+}
+
+ThemeSwitch = connect(mapStateToProps, mapDispatchToProps)(ThemeSwitch)
+
+
 export default ThemeSwitch
--- redux-test/src/index.js
+++ redux-test/src/index.js
@@ -5,6 +5,7 @@ import Header from './Header'
 import Content from './Content'
 import PropTypes from 'prop-types'
 import * as serviceWorker from './serviceWorker';
+import { Provider } from './react-redux'
 
 function createStore(reducer) {
     let state = null
@@ -34,14 +35,6 @@ const themeReducer = (state, action) => {
 const store = createStore(themeReducer)
 
 class Index extends Component {
-    static childContextTypes = {
-        store: PropTypes.object
-    }
-
-    getChildContext() {
-        return { store }
-    }
-
     render() {
         return (
             <div>
@@ -52,7 +45,10 @@ class Index extends Component {
     }
 }
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <Index />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
