2ba385148e0e19277f6e681eb0de0f07f3b01ee0
Oriel <1102386151@qq.com>
2019-06-21 19:00:55 +0800

    播放操作完成.

--- fm/src/containers/songListBox.js
+++ fm/src/containers/songListBox.js
@@ -2,20 +2,36 @@ import React, { Component } from 'react';
 import { Icon, Drawer } from 'antd';
 
 class songListBox extends Component {
+    constructor() {
+        super()
+        this.state = {
+            checkedSong: {},
+            isplayOperateDrawerShow: false,
 
+        }
+    }
+    // 修改抽屉状态.
+    changePlayOperateDrawerStatus(status, row) {
+        this.setState({
+            isplayOperateDrawerShow: status,
+            checkedSong: row ? row : {}
+        }, () => {
+            console.log(this.state)
+        })
+    }
     render() {
         const playOperateDrawerTitle =
             <div className='playOperateDrawerTitle'>
-                <img src={this.props.checkedSong.al && this.props.checkedSong.al.picUrl} alt='' />
+                <img src={this.state.checkedSong.al && this.state.checkedSong.al.picUrl} alt='' />
                 <div>
-                    <span>歌曲 : {this.props.checkedSong.name}</span>
-                    <span>{this.props.checkedSong.ar && this.props.checkedSong.ar.map((item, index) => { return item.name + " " })}</span>
+                    <span>歌曲 : {this.state.checkedSong.name}</span>
+                    <span>{this.state.checkedSong.ar && this.state.checkedSong.ar.map((item, index) => { return item.name + " " })}</span>
                 </div>
             </div>
         return (
             <div className='songListBox'>
                 {this.props.songListDetail.tracks && this.props.songListDetail.tracks.map((item, index) =>
-                    <div key={index} className='songListRow'>
+                    <div key={index} className='songListRow' onClick={() => this.changePlayOperateDrawerStatus(true, item)}>
                         <span>{index + 1}</span>
                         <div>
                             <span>{item.name}</span>
@@ -31,22 +47,43 @@ class songListBox extends Component {
                     title={playOperateDrawerTitle}
                     placement='bottom'
                     closable={false}
-                    onClose={() => this.changeSongPlayListStatus(false)}
-                    visible={this.state.isSongPlayListShow}
-                    height={500}
+                    onClose={() => this.changePlayOperateDrawerStatus(false)}
+                    visible={this.state.isplayOperateDrawerShow}
+                    height={530}
                     maskClosable={true}
                 >
-                    <div className='songPlayListDrawer'>
-                        {
-                            this.props.songPlayList.map((item, index) =>
-                                <p key={index} >
-                                    <span className={(item.id === this.props.nowPlaySongId) && "nowPlaySong"}>{item.name} - {item.ar.map((arItem, arIndex) => { return arItem.name + " " })}</span>
-                                    <Icon type="close" onClick={() => this.delSongPlayRow(item)} />
-                                </p>
-                            )
-                        }
+                    <div className='playOperateDrawer'>
+                        <p>
+                            <Icon type="play-circle" />
+                            <span>下一首播放</span>
+                        </p>
+                        <p>
+                            <Icon type="folder-add" />
+                            <span>收藏到歌单</span>
+                        </p>
+                        <p>
+                            <Icon type="download" />
+                            <span>下载</span>
+                        </p>
+                        <p>
+                            <Icon type="message" />
+                            <span>评论</span>
+                        </p>
+                        <p>
+                            <Icon type="share-alt" />
+                            <span>分享</span>
+                        </p>
+                        <p>
+                            <Icon type="user" />
+                            <span>歌手 : {this.state.checkedSong.ar && this.state.checkedSong.ar.map((arItem, arIndex) => { return arItem.name + " " })}</span>
+                        </p>
+                        <p>
+                            <Icon type="customer-service" />
+                            <span>专辑 : {this.state.checkedSong.al && this.state.checkedSong.al.name}</span>
+                        </p>
+
                     </div>
-                    <div className='songPlayListColseDrawer' onClick={() => this.changeSongPlayListStatus(false)}>关闭</div>
+                    <div className='songPlayListColseDrawer' onClick={() => this.changePlayOperateDrawerStatus(false)}>关闭</div>
                 </Drawer>
             </div>
         )
