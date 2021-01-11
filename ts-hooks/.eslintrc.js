188da6d0b7cc1dc0088553846e4e448b57904ea5
oriel <1102386151@qq.com>
2021-01-11 16:39:06 +0800

    完成。

--- ts-hooks/.eslintrc.js
+++ ts-hooks/.eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // 指定ESLint解析器
+    parser: '@typescript-eslint/parser',  // 指定ESLint解析器
     extends: [
-        'plugin:react/recommended', // 使用来自 @eslint-plugin-react 的推荐规则
-        'plugin:@typescript-eslint/recommended', // 使用来自@typescript-eslint/eslint-plugin的推荐规则
-        'prettier/@typescript-eslint', // 使用 ESLint -config-prettier 禁用来自@typescript-eslint/ ESLint 与 prettier 冲突的 ESLint 规则
+        'plugin:react/recommended',  // 使用来自 @eslint-plugin-react 的推荐规则
+        'plugin:@typescript-eslint/recommended',  // 使用来自@typescript-eslint/eslint-plugin的推荐规则
+        'prettier/@typescript-eslint',  // 使用 ESLint -config-prettier 禁用来自@typescript-eslint/ ESLint 与 prettier 冲突的 ESLint 规则
         'plugin:prettier/recommended',
     ],
     parserOptions: {
-        ecmaVersion: 2018, // 允许解析最新的 ECMAScript 特性
-        sourceType: 'module', // 允许使用 import
+        ecmaVersion: 2018,  // 允许解析最新的 ECMAScript 特性
+        sourceType: 'module',  // 允许使用 import
         ecmaFeatures: {
-            jsx: true, // 允许对JSX进行解析
+            jsx: true,  // 允许对JSX进行解析
         },
     },
     rules: {
@@ -19,7 +19,7 @@ module.exports = {
     },
     settings: {
         react: {
-            version: 'detect', // 告诉 eslint-plugin-react 自动检测 React 的版本
+            version: 'detect',  // 告诉 eslint-plugin-react 自动检测 React 的版本
         },
     },
 };
